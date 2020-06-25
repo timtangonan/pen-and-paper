6fd371a570114bd1a0937d05e0c9249a89b4141b
Tim Tangonan <trstangonan@gmail.com>
2020-06-25 18:20:38 +0800

    minor additon of round button edge

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,7 +8,7 @@ const IndexPage = () => {
       <div className="flex flex-col items-start justify-center h-full w-1/2 mx-auto">
         <h1 className="mt-4 font-bold text-2xl"><strong className="text-gray-800">Your one-stop shop for art supplies.</strong></h1>
         <p className="mt-4">Pen & Paper online store caters to your needs as a stationery junky. We completely understand the euphoria of smelling book pages and running your fingers over premium paper.</p>
-        <button className="py-2 px-6 my-6 bg-blue-700 text-white">
+        <button className="py-2 px-6 my-6 bg-blue-700 text-white rounded">
           <Link to="/products">Browse Shop</Link>
         </button>
         <Image
