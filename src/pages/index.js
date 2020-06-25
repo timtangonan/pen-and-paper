eb1f9c17443f513386832ca37f885ee151e2129c
Tim Tangonan <trstangonan@gmail.com>
2020-06-25 19:47:13 +0800

    footer link opens new tab and add new favicon

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,7 +5,7 @@ import { Image } from "cloudinary-react"
 const IndexPage = () => {
   return (
     <div className="h-full">
-      <div className="flex flex-col items-start justify-center h-full w-1/2 mx-auto">
+      <div className="flex flex-col items-start justify-center h-full mx-auto sm:w-full sm:px-2 md:w-2/3">
         <h1 className="mt-4 font-bold text-2xl"><strong className="text-gray-800">Your one-stop shop for art supplies.</strong></h1>
         <p className="mt-4">Pen & Paper online store caters to your needs as a stationery junky. We completely understand the euphoria of smelling book pages and running your fingers over premium paper.</p>
         <button className="py-2 px-6 my-6 bg-blue-700 text-white rounded">
